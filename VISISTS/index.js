20f86bee9b46f610424661efc1b820f651193564
DESKTOP-7E0JBA4\Meng <jimmylin0179@gmail.com>
2018-12-19 16:44:34 +0800

    Docker Compose finish

--- VISISTS/index.js
+++ VISISTS/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const redis = require('redis');
+const process = require('process');
 
 const app = express();
 const client = redis.createClient({
@@ -9,12 +10,22 @@ const client = redis.createClient({
 client.set('visits',0);
 
 app.get('/',(req,res) =>{
+    process.exit(0);    ///get crash  when visit this root route
     client.get('visits',(err,visits) =>{
         res.send('Number of visits is' + visits);
-        client.set('visists', parseInt(visits) + 1);
+        client.set('visits', parseInt(visits) + 1);
     })
 });
 
 app.listen(8081,() => {
     console.log('Listening on port 8081');
 })
+
+
+// docker-compose up 
+// docker-compose up --build
+
+//Launch in background
+    //docker0compose up -d
+//Stop Containers
+    //docker-compose down
